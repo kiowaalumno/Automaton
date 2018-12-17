83f46163385cf37cb28b3233da0b61bb13563ca6
Kiowa Alumno <kiowaalumno@gmail.com>
2018-12-17 09:47:07 -0600

    Added goalOrientedRobot

--- main.js
+++ main.js
@@ -65,6 +65,7 @@ console.log(next.parcels);
 console.log(first.place);
 // → Post Office
 
+// The Robot
 function runRobot(state, robot, memory) {
   for (let turn = 0;; turn++) {
     if (state.parcels.length == 0) {
@@ -82,6 +83,7 @@ function randomPick(array) {
   let choice = Math.floor(Math.random() * array.length);
   return array[choice];
 }
+// Random Robot
 
 function randomRobot(state) {
   return {direction: randomPick(roadGraph[state.place])};
@@ -109,6 +111,7 @@ const mailRoute = [
   "Grete's House", "Shop", "Grete's House", "Farm",
   "Marketplace", "Post Office"
 ];
+// Route Robot
 
 function routeRobot(state, memory) {
   if (memory.length == 0) {
@@ -116,9 +119,36 @@ function routeRobot(state, memory) {
   }
   return {direction: memory[0], memory: memory.slice(1)};
 }
+
+function findRoute(graph, from, to) {
+  let work = [{at: from, route: []}];
+  for (let i = 0; i < work.length; i++) {
+    let {at, route} = work[i];
+    for (let place of graph[at]) {
+      if (place == to) return route.concat(place);
+      if (!work.some(w => w.at == place)) {
+        work.push({at: place, route: route.concat(place)});
+      }
+    }
+  }
+}
+// Goal Oriented Robot
+
+function goalOrientedRobot({place, parcels}, route) {
+  if (route.length == 0) {
+    let parcel = parcels[0];
+    if (parcel.place != place) {
+      route = findRoute(roadGraph, place, parcel.place);
+    } else {
+      route = findRoute(roadGraph, place, parcel.address);
+    }
+  }
+  return {direction: route[0], memory: route.slice(1)};
+}
+
 // Running the application
 
-runRobot(VillageState.random(), randomRobot);
+runRobot(VillageState.random(), goalOrientedRobot, []);
 // → Moved to Marketplace
 // → Moved to Town Hall
 // → …
